076344a1ad4f80337c53af506828dddc5cca2c6f
Priyanshu Sharma <Priyanshu@email.com>
2026-01-21 09:59:55 +0530

    Initial commit

---
+++ Exp4.py
@@ -0,0 +1,24 @@
+# Exp-->4.1
+my_list = [10, 20, 30, 40]
+my_tuple = (10, 20, 30, 40)
+print("List elements:")
+for item in my_list:
+    print(item)
+print("Tuple elements:")
+for item in my_tuple:
+    print(item)
+
+
+
+# Exp----> 4.2
+# for loop
+sequence = [1, 2, 3, 4, 5]
+for i in sequence:
+    print(i)
+
+
+# while loop
+    num = int(input("Enter a number: "))
+while num >= 0:
+    print(num)
+    num -= 1
